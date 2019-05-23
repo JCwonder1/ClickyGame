be1dbf7051030b8d690c51bbfe0bb3ec94e75b43
Joseph Cannova <jcannova@wheelership.com>
2019-05-23 16:50:53 -0400

    Created react app

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+//TODO: Build a Nav Component
+//TODO: Build a Header Component
+//TODO: Build a Main Component
+//TODO: LOGIC : Declare and Empty Array
+//TODO: Build an Array of image names
+//TODO: Use the image name
+
