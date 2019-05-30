f31e7cc936eb9497b06719ef09d398929c880081
Joseph Cannova <jcannova@wheelership.com>
2019-05-30 17:57:27 -0400

    Created Nav component, declared score and count states and added functions to increase and set high score

---
+++ src/components/NavContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav(props) {
+  return (
+    <nav className="navbar navbar-dark fixed-top bg-primary">
+      <a className="navbar-brand" href="/">
+        Clicky Game
+      </a>
+      <p className="navbar-brand">Click on an image</p>
+      <p className="navbar-brand">Score:{props.count} Top Score: {props.score}</p>
+    </nav>
+  );
+}
+
+export default Nav;
