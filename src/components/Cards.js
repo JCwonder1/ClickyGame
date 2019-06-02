c1a356b91abb915a93a16f35689c54701bf7eb7f
Joseph Cannova <jcannova@wheelership.com>
2019-06-02 19:56:16 -0400

    Fixed Image Directory

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,7 +7,7 @@ function Cards(props){
       <div className="card click-item">
         <img
           className={"card-img " + props.divclass}
-          src={props.image}
+          src={".." + props.image}
           data-index={props.data}
           alt=""
           id={props.id}
