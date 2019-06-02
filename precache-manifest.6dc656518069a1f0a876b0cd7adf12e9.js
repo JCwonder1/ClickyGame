fad4d59e545a8dbf7db856388a6d7d56b70da1dd
Joseph Cannova <jcannova@wheelership.com>
2019-06-02 19:52:19 -0400

    Updates

---
+++ precache-manifest.6dc656518069a1f0a876b0cd7adf12e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8834b8fbe15a9759f084fa38fbf372ca",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "5f1e3f6b423d48e99c4d",
+    "url": "/ClickyGame/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "60b887e99f4ab792842e",
+    "url": "/ClickyGame/static/js/2.b88ea0fd.chunk.js"
+  },
+  {
+    "revision": "5f1e3f6b423d48e99c4d",
+    "url": "/ClickyGame/static/js/main.51d248d5.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
